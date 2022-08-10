3859bf0c26248c69bd2c29a56b5e814e0cf70d39
sujan mia <sujanfci@gmail.com>
2022-08-10 16:33:32 +0600

    first commit

---
+++ sujan.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <title>School management system</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+
+
+<!-- <div class="pic">></div> -->
+
+  <ul>
+  <li ><a href="#" class="active">Home</a></li>
+  <li  class="dropdown">
+    <a href="#" icon>Expertise</a>
+  <div class="dropdown-content">
+    <a href="">CSS</a>
+    <a href="">HtML</a>
+</div>
+  </li>
+  <li><a href="#">Hire me</a></li>
+  <li><a href="#">Contract us</a></li>
+
+</ul>
+
+
+
+
+<div class="content">
+<div class="left-content"></div>
+<div class="center-content"></div>
+<div class="right-content"></div>
+</div>
+
+ 
+ 
+ 
+</body>
+</html>
